f74ef512fb1a5634135eb75f9f608e65cb6a0a72
Nguyen Phuong Ly <22013360@st.phenikaa-uni.edu.vn>
2025-05-07 10:52:24 +0700

    Update run.sh

--- addons/run.sh
+++ addons/run.sh
@@ -9,9 +9,9 @@ docker run -d \
   --name face \
   --restart unless-stopped \
   --network host \
-  -v /data/facerekoginition/www/snapshots:/app/monitor \
-  -v /data/facerekoginition/config.json:/app/config.json \
-  -v /data/facerekoginition/logs:/app/logs \
+  -v /config/facerekoginition/snapshots:/app/monitor \
+  -v /config/facerekoginition/config.json:/app/config.json \
+  -v /config/facerekoginition/logs:/app/logs \
   rekognitionfacemanager:2.0
 
 echo "Container stopped."
