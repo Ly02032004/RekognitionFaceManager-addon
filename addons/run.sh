6cd0c671791751fac864290c68a5ee8275f73784
Nguyen Phuong Ly <22013360@st.phenikaa-uni.edu.vn>
2025-05-24 09:05:14 +0700

    Update run.sh

--- addons/run.sh
+++ addons/run.sh
@@ -3,13 +3,16 @@ set -e
 
 # Kéo image từ registry (Home Assistant tự động kéo dựa trên config.yaml)
 echo "Starting Docker container from registry..."
-mkdir /config/facerekoginition
+mkdir -p /config/facerekoginition
+if [ ! -f /config/facerekoginition/config.json ]; then
+  touch /config/facerekoginition/config.json
+fi
 # Chạy container với image đã định nghĩa
 docker run -d \
   --name face \
   --restart unless-stopped \
   --network host \
-  -v /config/facerekoginition/snapshots:/app/monitor \
+  -v /config/facerekoginition/www/snapshots:/app/monitor \
   -v /config/facerekoginition/config.json:/app/config.json \
   -v /config/facerekoginition/logs:/app/logs \
   rekognitionfacemanager:2.0
